b531f6fb1aafd5c8543c9363d918136954d10196
tpavay <pavayt@gmail.com>
2025-02-08 18:11:14 -0500

    Initial Commit

---
+++ Ascend/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Ascend
+//
+//  Created by Tyler Pavay on 2/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
